5330f5ecfd9c35f323e2100a1923fb1626e6f6be
Bekimoon0043 <bereketamare0043@gmail.com>
2026-08-15 07:46:09 +0300

    Add frontend core: main, App, styles, types env

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import TopBar from './components/Layout/TopBar';
+import LeftPanel from './components/Layout/LeftPanel';
+import RightPanel from './components/Layout/RightPanel';
+import Scene from './components/Viewport/Scene';
+import { useAuth } from './hooks/useAuth';
+import { useKeyboardShortcuts } from './hooks/useKeyboardShortcuts';
+
+export default function App() {
+  const { user } = useAuth();
+  useKeyboardShortcuts();
+
+  return (
+    <div className="h-screen w-screen flex flex-col bg-[#141519]">
+      <TopBar user={user} />
+      <div className="flex-1 flex overflow-hidden">
+        <LeftPanel />
+        <main className="flex-1 relative">
+          <Scene />
+        </main>
+        <RightPanel />
+      </div>
+    </div>
+  );
+}
